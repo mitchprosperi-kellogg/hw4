79b6864ad270bd0a22e8b0bdb577feb2c55275e5
mitchprosperi-kellogg <mitch.prosperi@kellogg.northwestern.edu>
2023-03-04 22:22:31 +0000

    Adding login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    @user["username"] = params["user"]["first_name"]
+    ##@user["last_name"] = params["user"]["last_name"]
     @user["email"] = params["user"]["email"]
     @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
