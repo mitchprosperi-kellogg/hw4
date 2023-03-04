5bff942eba0c260f5d2800033e9817b60ffed0b4
mitchprosperi-kellogg <mitch.prosperi@kellogg.northwestern.edu>
2023-03-04 22:02:25 +0000

    Initial commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,26 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+  
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
   end
 
   def create
+    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post.save
-    redirect_to "/places/#{@post["place_id"]}"
+  else
+    flash["notice"] = "Login first."
+  end
+  redirect_to "/places/#{@post["place_id"]}"
   end
 
 end
