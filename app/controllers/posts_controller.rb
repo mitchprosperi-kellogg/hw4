5ab715d485a239bf6a85202c83ea186ecd8c1f52
mitchprosperi-kellogg <mitch.prosperi@kellogg.northwestern.edu>
2023-03-08 03:56:52 +0000

    Navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,13 +13,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    if @current_user
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post.save
+      if @current_user
+        @post = Post.new
+        @post["description"] = params["post"]["description"]
+        @post.uploaded_image.attach(params["post"]["uploaded_image"])
+        @post["user_id"] = @current_user["id"]
+        @post.save
   else
     flash["notice"] = "Login first."
   end
